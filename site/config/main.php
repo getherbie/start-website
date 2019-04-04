19df020e038239506b4a66970954c463e9570edd
tbreuss <thomasbreuss@gmx.ch>
2019-04-04 20:14:57 +0200

    Install markdown, php config

---
+++ site/config/main.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'language' => 'en',
+    'locale' => 'en_EN',
+    'niceUrls' => true,
+    'fileExtensions' => [
+        'layouts' => 'twig',
+    ],
+    'enabledPlugins' => 'simplecontact,simplesearch',
+    'enabledSysPlugins' => 'markdown,imagine'
+];
