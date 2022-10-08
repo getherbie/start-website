769bb88ea7712351a4133fa680ff0b9e8ce2b45c
tbreuss <thomasbreuss@gmx.ch>
2022-10-08 10:20:47 +0200

    fix: remove config entry, cleanup composer.json

--- site/config/main.php
+++ site/config/main.php
@@ -4,9 +4,6 @@ return [
     'language' => 'en',
     'locale' => 'en_EN',
     'niceUrls' => true,
-    'fileExtensions' => [
-        'layouts' => 'twig',
-    ],
     'enabledPlugins' => 'simplecontact,simplesearch',
     'enabledSysPlugins' => 'twig_core,twig_plus,markdown,imagine'
 ];
